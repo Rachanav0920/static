120276f27c6740508fcdf9a122be4e6211005376
Rachanav <rachanavenkatesh96@gmail.com>
2021-05-24 17:10:12 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from app1 import views
+app_name="app1"
+
+urlpatterns = [
+    path('index/',views.index,name="index"),
+    path('demo1/',views.demo1,name="demo1"),
+    path('<a>/<b>',views.demo2,name="demo2"),
+    path('demo3',views.demo3,name="demo3"),
+    path('',views.demo5,name="demo5"),
+ 
+]
---
+++ app1/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Welcome to app1.index page....</h1>")
+
+def demo1(request):
+    return render(request,"demo1.html",{"name":"Rachana"})
+
+def demo2(request,a,b):
+    return render (request,"demo2.html",{'a':int(a),'b':int(b)})
+
+def demo3(request):
+    return render(request,"demo3.html",{'fruits':['apple','banana','orange','mango']})
+
+#def demo4(request):
+ #   det={'name':"Rachana","age":24,"Desig":"Associate Software Engineer"}
+ #   return render(request,"demo3.html",{'fruits':['apple','banana','orange','mango'],'det':det})
+
+def demo5(request):
+    return render(request,"Sample2.html")   
